53231404bd1185b2286eb3137a055dd00b000dbe
Tse Kit Yam <me@kytse.com>
2018-07-13 17:37:58 +0800

    add react-big-calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import * as moment from "moment";
+import BigCalendar from "react-big-calendar";
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <BigCalendar />
       </div>
     );
   }
